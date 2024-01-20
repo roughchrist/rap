458940e2c774cbec42b60e77f8b4a47929765a06
Administrator <Administrator@COMLAB09>
2024-01-20 11:54:38 +0700

    Add project files.

---
+++ rap/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Dim user As String = "Admin"
+    Dim pass As String = "Admin123"
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim userInp As String = TextBox1.Text
+        Dim passInp As String = TextBox2.Text
+
+        If user = userInp And pass = passInp Then
+            Form2.Show()
+            Me.Close()
+        Else
+            MessageBox.Show("Invalid Credentials.")
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+        End If
+
+    End Sub
+End Class
---
+++ rap/Form3.vb
@@ -0,0 +1,104 @@
+﻿Public Class Form3
+
+    Dim first As Double
+    Dim second As Double
+    Dim operand As String
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Text = TextBox1.Text & sender.text
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text & sender.text
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        TextBox1.Text = TextBox1.Text & sender.text
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        TextBox1.Text = TextBox1.Text & sender.text
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        TextBox1.Text = TextBox1.Text & sender.text
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        TextBox1.Text = TextBox1.Text & sender.text
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        TextBox1.Text = TextBox1.Text & sender.text
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        TextBox1.Text = TextBox1.Text & sender.text
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        TextBox1.Text = TextBox1.Text & sender.text
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        TextBox1.Text = TextBox1.Text & sender.text
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        Dim Result As Double
+        second = Val(TextBox1.Text)
+
+        Select operand
+            Case "+"
+                Result = first + second
+                TextBox1.Text = Result.ToString()
+            Case "-"
+                Result = first - second
+                TextBox1.Text = Result.ToString()
+            Case "/"
+                Result = first / second
+                TextBox1.Text = Result.ToString()
+            Case "*"
+                Result = first * second
+                TextBox1.Text = Result.ToString()
+        End Select
+
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button17_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button17.Click
+        Form2.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        first = Val(TextBox1.Text)
+        TextBox1.Text = ""
+        TextBox1.Focus()
+        operand = "+"
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        first = Val(TextBox1.Text)
+        TextBox1.Text = ""
+        TextBox1.Focus()
+        operand = "-"
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        first = Val(TextBox1.Text)
+        TextBox1.Text = ""
+        TextBox1.Focus()
+        operand = "*"
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        first = Val(TextBox1.Text)
+        TextBox1.Text = ""
+        TextBox1.Focus()
+        operand = "/"
+    End Sub
+End Class
